679a0b6229cc53de4997e6023023a867f4e54a46
Lara Ehrenhofer <lara.ehrenhofer@gmail.com>
2020-09-16 11:05:54 +0200

    changed movie recommendation algorithm

--- recommender.py
+++ recommender.py
@@ -1,29 +1,67 @@
 """machine-learning Code that returns movies"""
 import random
+import numpy as np
 
-MOVIES = ['Karate Kid',
-          'Toy Story 1',
+MOVIES = ['Toy Story 1',
           'Toy Story 2',
           'Toy Story 3',
-          'Toy Story 4',
-          'Star Wars 1',
-          'Star Wars 2',
-          'Star Wars 3',
-          'Star Wars 4',
-          'Star Wars 5',
-          'Star Wars 6',
+          'Star Wars',
           'Jurassic Park',
           'Dawn of the Dead',
-          'Home Alone']
+          'Home Alone',
+          'A Million ways to die in the west',
+          'Blade Runner 2049',
+          'Cats',
+          'Dune',
+          'Eurovision Song Contest: The Story Of Fire Saga',
+          'Gattaca',
+          'Green Mile',
+          'Harry Potter and the deathly hallows ',
+          'Hidden Figures',
+          'High School Musical',
+          'High School Musical 2',
+          'Ice Age',
+          'Inside Out',
+          'Karate Kid (1984)',
+          'Lord of the Rings -- The Fellowship of the Ring',
+          'Lord of the Rings -- The Return of the King',
+          'Lord of the Rings -- The Two Towers',
+          'Matrix',
+          'Memento',
+          'Moana',
+          'Red Joan',
+          "Romy and Michelle's High School Reunion",
+          'Se7en',
+          'Sharknado',
+          'Shoplifters',
+          'Shutter Island',
+          'Smiley Face',
+          'Ted',
+          'The Big Lebowksi',
+          'The Big Lebowski',
+          'The Body of Truth',
+          'The Breakfast Club',
+          'The Empire strikes back',
+          'The Farewell',
+          'The Notebook',
+          'The Sixth Sense',
+          'What happened to Monday']
+
+ranges =list(np.arange(1, step=.02))
+
+probs = random.sample(ranges, k=len(MOVIES))
+
 
 def random_recommend(num):
     """Simple recommender that return N movies. Wow!
     """
-    result = random.sample(MOVIES, k=num)
+    result = random.choices(MOVIES, k=num, weights=probs)
     return result
+
 
 def nmf():
     ...
+
 
 def deep_recommend():
     ...
